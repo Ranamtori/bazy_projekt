1590c9e9a84eaf6107b645ecc0965ce544f3d2eb
Julia <juliasara898@gmail.com>
2023-04-26 19:59:35 +0200

    first commit uwu

---
+++ src/test/java/pl/schronisko/zwierzatkainfo/ZwierzatkainfoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.schronisko.zwierzatkainfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwierzatkainfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
