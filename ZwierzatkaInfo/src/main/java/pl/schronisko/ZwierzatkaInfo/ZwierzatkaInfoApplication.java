8a0c4ab2bc03b756b03b57b19fcdfc309a57ad77
Ola <ola.pabian@interia.com>
2023-04-27 21:08:01 +0200

    podejscie drugie kill me

---
+++ ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/ZwierzatkaInfoApplication.java
@@ -0,0 +1,13 @@
+package pl.schronisko.ZwierzatkaInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwierzatkaInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwierzatkaInfoApplication.class, args);
+	}
+
+}
---
+++ ZwierzatkaInfo/src/test/java/pl/schronisko/ZwierzatkaInfo/ZwierzatkaInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.schronisko.ZwierzatkaInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwierzatkaInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
