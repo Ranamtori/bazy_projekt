29bd7b3a03965f93a56ab611439117c6f559b9c0
Ola <ola.pabian@interia.com>
2023-04-27 22:54:01 +0200

    home page

---
+++ ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/HomeControler.java
@@ -0,0 +1,16 @@
+package pl.schronisko.ZwierzatkaInfo.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeControler {
+    @GetMapping("/")
+    @ResponseBody
+    public String home()
+    {
+        return "home page";
+    }
+
+}
