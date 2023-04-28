ed0edb4a2f40e04efaf72a909a618fd8000112c4
Ola <ola.pabian@interia.com>
2023-04-28 23:37:59 +0200

    stworzenie klasy zwierze pierwsze polaczenie backend-frontend

--- ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/HomeControler.java
+++ ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/HomeControler.java
@@ -1,15 +1,26 @@
 package pl.schronisko.ZwierzatkaInfo.controlers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import pl.schronisko.ZwierzatkaInfo.model.Zwierze;
 
 @Controller
 public class HomeControler {
     @GetMapping("/")
     //@ResponseBody //to jest po to zeby to co w return sie zamienilo na html a jak w return jest plik html to nie trzeba tego
-    public String home()
+//    public String home() //zwraca dokument html
+//    {
+//        return "index";
+//    }
+//    public Zwierze home() //zwraca obiekt
+//    {
+//        return new Zwierze(12,"Pucek",4,'k','m',"dachowiec","czarny","maly",true,false,true,true);
+//    }
+    public String home(Model model) //dodaje atrybut do modela zeby za pomaca Thymeleaf polaczyc z html
     {
+        model.addAttribute("zwierze",new Zwierze(12,"Pucek",4,'k','m',"dachowiec","czarny","maly",true,false,true,true));
         return "index";
     }
 }
--- ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/model/Zwierze.java
+++ ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/model/Zwierze.java
@@ -1,4 +1,27 @@
 package pl.schronisko.ZwierzatkaInfo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//wykorzystujac lombock ustawiam gonstruktory settery i gettery
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Zwierze {
+    public int Id;
+    public String Imie;
+    public int NrKojca;
+    public char Gatunek;//p/k
+    public char Plec;//k/m
+    public String Rasa;
+    public String Umaszczenie;
+    public String Wielkosc; //maly/sredni/duzy
+    public boolean CzyDoAdopcji;
+    public boolean CzyPsy; //czy moze zyc zpsami
+    public boolean CzyKoty;
+    public boolean CzyDzieci;
+
 }
