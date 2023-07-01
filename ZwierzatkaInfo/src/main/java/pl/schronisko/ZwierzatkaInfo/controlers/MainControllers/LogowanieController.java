b896790f22baf2adbb80eb3c3e8034473374b259
Julietta <juliasara898@gmail.com>
2023-07-01 12:39:32 +0200

    wyskakuje blad przy zlym hasle

--- ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/MainControllers/LogowanieController.java
+++ ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/MainControllers/LogowanieController.java
@@ -2,8 +2,11 @@ package pl.schronisko.ZwierzatkaInfo.controlers.MainControllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import pl.schronisko.ZwierzatkaInfo.repository.HasloRepository;
+
+import java.text.AttributedString;
 
 @Controller
 @RequestMapping("/logowanie")
@@ -17,22 +20,24 @@ public class LogowanieController {
         this.hasloRepository = hasloRepository;
     }
 //funkcje do wyswietlanie stron
-    @RequestMapping(method = RequestMethod.GET)
-    public String logowaniePage() {
-        return "mainview/logowanie";
-    }
 
-    @GetMapping("/adminHome")
-    public String adminHomePage() {
+    @RequestMapping(value = "/adminhomemain", method = RequestMethod.GET)
+    public String loginPage() {
         return "adminview/adminHome";
     }
 
+    @GetMapping("/logowanie")
+    public String adminHomePage() {
+        return "mainview/logowanie";
+    }
+
     @PostMapping("/submitForm")
-    public String submitForm(@RequestParam("password") String password) {
+    public String submitForm(@RequestParam("password") String password, Model model) {
         this.haslo = password;
         if (haslo.equals("haslo")) {
             return "redirect:/adminHome";
         } else {
+            model.addAttribute("error", "Błędne hasło");
             return "redirect:/logowanie";
         }
     }
