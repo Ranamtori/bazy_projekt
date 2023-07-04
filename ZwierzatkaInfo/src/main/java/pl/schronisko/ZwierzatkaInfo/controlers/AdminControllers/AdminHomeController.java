2dfd20233a071d08d4681fa7fbda0f251742078d
Julietta <juliasara898@gmail.com>
2023-07-05 00:32:47 +0200

    ale teraz jest ladnie i dziala

--- ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/AdminControllers/AdminHomeController.java
+++ ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/AdminControllers/AdminHomeController.java
@@ -4,10 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import pl.schronisko.ZwierzatkaInfo.model.Opiekunowie;
 import pl.schronisko.ZwierzatkaInfo.model.Zdrowie;
 import pl.schronisko.ZwierzatkaInfo.model.Zwierze;
@@ -29,16 +26,19 @@ public class AdminHomeController {
     private Zwierze z1; //ostatnio dodane zwierze
     private List<Zwierze> zwierzeta;
     private Opiekunowie existingOpiekun;
+
     @Autowired
     public AdminHomeController(ZwierzeRepository zwierzeRepository, OpiekunRepository opiekunRepository, ZdrowieRepository zdrowieRepository) {
         this.zwierzeRepository = zwierzeRepository;
         this.opiekunRepository = opiekunRepository;
         this.zdrowieRepository = zdrowieRepository;
         this.zwierzeta = new ArrayList<>(); // inicjalizacja listy zwierzeta
-        this.existingOpiekun=new Opiekunowie();
+        this.existingOpiekun = new Opiekunowie();
     }
     @GetMapping("/adminHome")
-    private String adminPage(){
+    private String adminPage(Model model) {
+        List<Opiekunowie> opiekunOptional = opiekunRepository.findAll();
+        model.addAttribute("opiekunList",opiekunOptional);
         return "adminview/adminHome";
     }
     //metody:
@@ -49,13 +49,13 @@ public class AdminHomeController {
     //patch (tak jak put) ale nie przesyla calego obiektu tylk odane do aktualizacji
 
     @PostMapping("/adminHomeZwierze")
-    private String addZwierze(Zwierze zwierze)
-    {
+    private String addZwierze(Zwierze zwierze) {
         //po przechwyceniu danych z formularza dodajemy do listy z home controller
-        z1=zwierze;
+        z1 = zwierze;
         zwierzeRepository.save(zwierze);
         return "redirect:/addZdrowie"; //sie wykona ta home po tym
     }
+
     @PostMapping("/adminHomeZdrowie")
     private String addZdrowie(Zdrowie zdrowie) {
         zdrowie.setZwierze(z1);
@@ -66,15 +66,14 @@ public class AdminHomeController {
         return "redirect:/addOpiekun";
     }
 
-    @GetMapping ("/adminHomeWybranyOpiekun")
-    private String addWybranyOpiekun(@RequestParam("id_opiekuna") Long id, Model model)
-    {        // Pobierz zwierze o podanym id
+    @GetMapping("/adminHomeWybranyOpiekun")
+    private String addWybranyOpiekun(@RequestParam("id_opiekuna") Long id, Model model) {        // Pobierz zwierze o podanym id
         Optional<Opiekunowie> opiekunowieOptional = opiekunRepository.findById(id);
         if (opiekunowieOptional.isPresent()) {
             Opiekunowie opiekunowie = opiekunowieOptional.get();
             model.addAttribute("opiekun", opiekunowie);
-            opiekunowie.setIloscPodOpieka(opiekunowie.getIloscPodOpieka()+1);
-            zwierzeta=opiekunowie.getZwierzeta();
+            opiekunowie.setIloscPodOpieka(opiekunowie.getIloscPodOpieka() + 1);
+            zwierzeta = opiekunowie.getZwierzeta();
             zwierzeta.add(z1);
             opiekunowie.setZwierzeta(zwierzeta);
             z1.setOpiekun(opiekunowie);
@@ -83,15 +82,28 @@ public class AdminHomeController {
         }
         return "redirect:/adminHome"; //sie wykona ta home po tym
     }
+
     @PostMapping("/adminHomeOpiekun") //dodawanie nowego opiekuna
-    private String addOpiekun(Opiekunowie opiekun)
-    {
-            opiekun.setIloscPodOpieka(0);
-            opiekun.setZwierzeta(null);
-            opiekunRepository.save(opiekun);
-            return "redirect:/adminHome"; //sie wykona ta home po tym
+    private String addOpiekun(Opiekunowie opiekun) {
+        opiekun.setIloscPodOpieka(0);
+        opiekun.setZwierzeta(null);
+        opiekunRepository.save(opiekun);
+        return "redirect:/adminHome"; //sie wykona ta home po tym
     }
 
+    @PostMapping("/usunOpiekun")
+    public String usunOpiekun(@RequestParam("idOpiekuna") Long id, Model model) {
+        if (id != null) {
+            Optional<Opiekunowie> opiekunOptional = opiekunRepository.findById(id);
+            if (opiekunOptional.isPresent()) {
+                Opiekunowie opiekunowie2 = opiekunOptional.get();
+                if (opiekunowie2.getIloscPodOpieka()== 0) { //jesli nie ma zwierzat
+                    opiekunRepository.delete(opiekunowie2);
+                }
+            }
+        }
 
+
+        return "redirect:/adminHome";
+    }
 }
-
--- ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/MainControllers/MainviewController.java
+++ ZwierzatkaInfo/src/main/java/pl/schronisko/ZwierzatkaInfo/controlers/MainControllers/MainviewController.java
@@ -1,20 +1,33 @@
 package pl.schronisko.ZwierzatkaInfo.controlers.MainControllers;
-
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import pl.schronisko.ZwierzatkaInfo.model.Opiekunowie;
+import pl.schronisko.ZwierzatkaInfo.repository.OpiekunRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/")
 public class MainviewController {
-    @RequestMapping(method = RequestMethod.GET)
-    public String mainPage() {
-        return "mainview/mainview";
+    private final OpiekunRepository opiekunRepository;
+
+    public MainviewController(OpiekunRepository opiekunRepository) {
+        this.opiekunRepository = opiekunRepository;
     }
 
     @RequestMapping(value = "/logowaniemain", method = RequestMethod.GET)
     public String loginPage() {
         return "mainview/logowanie";
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String mainPage(Model model) {
+        List<Opiekunowie> opiekunOptional = opiekunRepository.findAll();
+        model.addAttribute("opiekunList",opiekunOptional);
+
+        return "mainview/mainview";
     }
 
 }
